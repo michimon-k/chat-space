a93725d401564d536cdd8ab6dcdc8469f43901ae
michimon-k <ksfgmichi@gmail.com>
2020-01-16 15:00:37 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# ルーティングを追加
 Rails.application.routes.draw do
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  devise_for :users
+  root to:'messages#index'
+  resources :users, only: [:edit, :update]
 end
